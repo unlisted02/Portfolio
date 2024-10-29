f50f2efd1ccf665cf8baa0f2ebd692b1a77d6af6
Unlisted <96585961+unlisted02@users.noreply.github.com>
2024-10-29 13:23:01 +0300

    Update Projects.js

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -51,6 +51,8 @@ Movie Recommendation System with Complete End-to-End Pipeline, Model Intregratio
               title="E-Learning"
               description="The project involves the creation of an e-learning website using HTML, CSS, Bootstrap 5, and JavaScript. The website aims to provide a user-friendly and responsive platform for learners to access educational content. Unlisted Academy creates an interactive and visually appealing learning experience."
               ghLink="https://github.com/unlisted02/E-Learning"
+              demoLink="https://e-learning-delta-opal.vercel.app/"
+              
                             
             />
           </Col>
